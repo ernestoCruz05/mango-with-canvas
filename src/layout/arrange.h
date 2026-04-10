1570916be78dfe7551354724ab2e2182b33ca16d
ernestoCruz05 <a2023144672@isec.pt>
2026-04-10 11:53:00 +0100

    dwindle resizewin fix

--- src/layout/arrange.h
+++ src/layout/arrange.h
@@ -1,5 +1,8 @@
 #include <assert.h>
 
+static void dwindle_resize_client_step(Monitor *m, Client *c, int32_t dx,
+				       int32_t dy);
+
 void save_old_size_per(Monitor *m) {
 	Client *c = NULL;
 
@@ -708,6 +711,8 @@ void resize_tile_client(Client *grabc, bool isdrag, int32_t offsetx,
 		resize_tile_scroller(grabc, isdrag, offsetx, offsety, time, false);
 	} else if (current_layout->id == VERTICAL_SCROLLER) {
 		resize_tile_scroller(grabc, isdrag, offsetx, offsety, time, true);
+	} else if (current_layout->id == DWINDLE) {
+		dwindle_resize_client_step(grabc->mon, grabc, offsetx, offsety);
 	}
 }
 
